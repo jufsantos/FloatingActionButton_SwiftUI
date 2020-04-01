65b51b6e039865f2bb713e8d4121fcafb7c343bb
julia <julia.fsantos2@gmail.com>
2020-04-01 16:27:32 -0300

    projeto teste

--- FloatingActionButton_Test/FloatingActionButton_Test/ContentView.swift
+++ FloatingActionButton_Test/FloatingActionButton_Test/ContentView.swift
@@ -14,8 +14,35 @@ struct Item {
 }
 
 struct ContentView: View {
+    @State private var items = [Item]()
+    
     var body: some View {
-        Text("Hello, There!")
+        ZStack {
+            List(self.items, id: \.uuid) { item in
+                Text(item.value)
+            }
+            VStack{
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    
+                    Button(action: {
+                    self.items.append(Item(value: "Item"))
+                    }, label: {
+                        Text("+")
+                        .font(.system(.largeTitle))
+                            .frame(width: 77, height: 70, alignment: .center)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 7)
+                    })
+                        .background(Color.blue)
+                        .cornerRadius(38.5)
+                        .padding()
+                        .shadow(color: Color.black.opacity(0.3), radius: 3, x: 3, y: 3)
+                }
+            }
+        }
     }
 }
 
